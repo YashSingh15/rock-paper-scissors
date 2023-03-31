45ac092ef87a29e76a97d46b7372f8dd38912787
Yash Singh <yashpsingh.edu@gmail.com>
2023-03-31 22:49:12 +0530

    Add capitalizeFirstLetter and playRound functions to handle game logic

--- javascript.js
+++ javascript.js
@@ -4,3 +4,35 @@ function getComputerChoice() {
     randomChoice = choices[randomChoiceIndex];
     return randomChoice;
 }
+
+function capitalizeFirstLetter(inputString) {
+    if (typeof inputString !== 'string') {
+        return '';
+    } 
+
+    return inputString[0].toUpperCase() + inputString.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = capitalizeFirstLetter(playerSelection);
+    let result = '';
+
+    if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock')) {
+        result = `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else if ((playerSelection === 'Scissors' && computerSelection === 'Rock') ||
+        (playerSelection === 'Rock' && computerSelection === 'Paper') ||
+        (playerSelection === 'Paper' && computerSelection === 'Scissors')) {
+        result = `You lose! ${computerSelection} beats ${playerSelection}!`;
+    } else {
+        result = `It's a tie! You both played ${playerSelection}`;
+    }
+
+    return result;
+}
+
+// all the ways that the player could lose
+// rock and scissors
+// scissors and paper
+// paper and rock
